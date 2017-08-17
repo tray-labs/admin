9f4938e2d3efcca1e0509ea397e3546f80522576
dandrade <dandrade@tray.net.br>
2017-08-17 14:30:07 -0300

    Adicionado redirecionamento para page404

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,21 +1,24 @@
 <template>
-  <router-view></router-view>
+    <router-view></router-view>
 </template>
 
 <script>
-export default {
-  name: 'app'
+    export default {
+        name: 'app',
+        mounted() {
+            this.checkPageExist();
+        },
+        watch : {
+            '$route' () {
+                this.checkPageExist();
+            }
+        },
+        methods: {
+            checkPageExist: function () {
+                if(!this.$route.matched.length) {
+                    return this.$router.push('/pages/404');
+                }
+            }
+        }
 }
 </script>
-
-<!--
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
